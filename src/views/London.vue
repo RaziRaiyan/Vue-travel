496cdba9395c58b4c0ba6faae7c9fe5353a5f64d
Raiyan Razi <raiyanrazi@Raiyans-MacBook-Pro.local>
2021-01-28 16:36:24 +0530

    added p to london

--- src/views/London.vue
+++ src/views/London.vue
@@ -3,6 +3,9 @@
     <h2>
       London
     </h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eligendi eos neque odio pariatur quis recusandae soluta voluptatibus! Doloremque et, eum in nesciunt odit quam? Asperiores corporis dignissimos iste nihil?
+    </p>
   </div>
 </template>
 
